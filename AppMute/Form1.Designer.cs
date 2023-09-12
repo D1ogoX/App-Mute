2ea2d340bb7948db3874667b7e64620738792341
Diogo Santiago <66495022@galp.com>
2023-09-12 15:33:37 +0100

    Add project files.

---
+++ AppMute/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace AppMute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::AppMute.Properties.Resources.mute;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(100, 100);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AppMute/Form1.cs
@@ -0,0 +1,117 @@
+﻿using AppMute.Properties;
+using Microsoft.Win32;
+using NAudio.CoreAudioApi;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AppMute
+{
+    public partial class Form1 : Form
+    {
+        private string AppName = "FMute";
+        private MMDevice device;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Size = new Size(50, 50);
+            this.MinimumSize = new Size(50, 50);
+            this.MaximumSize = new Size(50, 50);
+
+            this.StartPosition = FormStartPosition.Manual;
+            int x = Screen.PrimaryScreen.WorkingArea.Left;
+            int y = Screen.PrimaryScreen.WorkingArea.Bottom - this.Height;
+            this.Location = new Point(x, y);
+
+            using (var enumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator())
+            {
+                device = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            }
+
+            SetStartup();
+
+            checkMute_unmute();
+
+            SendNotification("FMute is running");
+        }
+
+        private void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            rk.SetValue(AppName, Application.ExecutablePath);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            mute_unmute();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control)
+            {
+                switch(e.KeyCode)
+                {
+                    case Keys.M:
+
+                        mute_unmute();
+
+                        break;
+                }
+            }
+        }
+
+        private void mute_unmute()
+        {
+            if (device.AudioEndpointVolume.Mute)
+            {
+                device.AudioEndpointVolume.Mute = false; //or false
+                pictureBox1.Image = Resources.mute;
+                SendNotification("Unmuted");
+            }
+
+            else
+            {
+                device.AudioEndpointVolume.Mute = true;
+                pictureBox1.Image = Resources.unmute;
+                SendNotification("Muted");
+            }
+        }
+
+        private void checkMute_unmute()
+        {
+            if (device.AudioEndpointVolume.Mute)
+            {
+                pictureBox1.Image = Resources.unmute;
+            }
+
+            else
+            {
+                pictureBox1.Image = Resources.mute;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            device.AudioEndpointVolume.Mute = false;
+
+            Debug.WriteLine(" > UNMUTE");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            device.AudioEndpointVolume.Mute = false;
+
+            Debug.WriteLine(" > UNMUTE");
+        }
+
+        private void SendNotification(string text)
+        {
+        }
+    }
+}
